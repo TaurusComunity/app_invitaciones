835e40c3fbc94c752137c30b0395fba693f6b742
JhoannS <sebastianzamudio2004@gmail.com>
2024-11-03 22:37:40 -0500

    preparando deploid en heroku

--- src/keys.js
+++ src/keys.js
@@ -1,10 +1,10 @@
 module.exports = {
     database: {
-      host: process.env.DB_HOST,
-      user: process.env.DB_USER,
-      password: process.env.DB_PASSWORD,
+      host: process.env.DB_HOST || '127.0.0.1',
+      user: process.env.DB_USER || 'root',
+      password: process.env.DB_PASSWORD || '',
       port: process.env.DB_PORT || 3306,
-      database: process.env.DB_NAME,
+      database: process.env.DB_NAME || 'invitaciones',
     },
   };
   
