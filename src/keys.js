2e908ea314a0b939ec091d709f6ebc89275ce384
JhoannS <sebastianzamudio2004@gmail.com>
2024-11-03 21:43:54 -0500

    creando archivo .env para variables de entorno

--- src/keys.js
+++ src/keys.js
@@ -1,9 +1,9 @@
 module.exports = {
     database: {
-        host: process.env.DB_HOST || 'localhost',
-        user: process.env.DB_USER || 'root',
-        password: process.env.DB_PASSWORD || '',
-        port: process.env.DB_PORT || 3306,
-        database: process.env.DB_NAME || 'invitaciones'
+        host:       process.env.DB_HOST || 'localhost',
+        user:       process.env.DB_USER || 'root',
+        password:   process.env.DB_PASSWORD || '',
+        port:       process.env.DB_PORT || 3306,
+        database:   process.env.DB_NAME || 'invitaciones'
     }
 };
